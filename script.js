678de5b17268291fc224be11ac035646daff06dc
Anthony McMorris <amcmorr2@asu.edu>
2025-09-28 03:12:36 +0000

    updates

--- script.js
+++ script.js
@@ -2,6 +2,11 @@ const form = document.getElementById('checkInForm');
 const nameInput = document.getElementById("attendeeName");
 const teamSelect = document.getElementById("teamSelect");
 
+// Track Attendance
+let count = 0;
+const maxCount = 50;
+
+
 // Handle form submission
 form.addEventListener("submit", function (event) {
 event.preventDefault(); // Prevent default form submission
@@ -9,9 +14,16 @@ event.preventDefault(); // Prevent default form submission
 // Get form values
 const name  = nameInput.value;  
 const team = teamSelect.value;
-const teamName = teamSelect.selectOptions [0].text;
+const teamName = teamSelect.selectedOptions [0].text;
 
 console.log(name, teamName);
+
+//Increment Count
+count++;
+console.log("Total check-ins:", count);
+
+// Update Progress Bar
+const percentage = Math.round((count / maxCount) * 100) + "%";
 
 });
 
