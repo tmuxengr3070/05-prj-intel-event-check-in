9b282330fcf087e680669762ef9453aa2d7d64cb
Anthony McMorris <amcmorr2@asu.edu>
2025-09-28 02:46:47 +0000

    updates

---
+++ script.js
@@ -0,0 +1,19 @@
+const form = document.getElementById('checkInForm');
+const nameInput = document.getElementById("attendeeName");
+const teamSelect = document.getElementById("teamSelect");
+
+// Handle form submission
+form.addEventListener("submit", function (event) {
+event.preventDefault(); // Prevent default form submission
+
+// Get form values
+const name  = nameInput.value;  
+const team = teamSelect.value;
+
+console.log(name, team);
+
+});
+
+  
+  
+
