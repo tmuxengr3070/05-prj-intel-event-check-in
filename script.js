323145ddba04c32ed97304b639052c93bfed0e5a
Anthony McMorris <amcmorr2@asu.edu>
2025-09-29 00:39:14 +0000

    Updates

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const form = document.getElementById('checkInForm');
+const form = document.getElementById("checkInForm");
 const nameInput = document.getElementById("attendeeName");
 const teamSelect = document.getElementById("teamSelect");
 
@@ -6,48 +6,39 @@ const teamSelect = document.getElementById("teamSelect");
 let count = 0;
 const maxCount = 50;
 
-
 // Handle form submission
 form.addEventListener("submit", function (event) {
-event.preventDefault(); // Prevent default form submission
+  event.preventDefault(); // Prevent default form submission
 
-// Get form values
-const name  = nameInput.value;  
-const team = teamSelect.value;
-const teamName = teamSelect.selectedOptions [0].text;
+  // Get form values
+  const name = nameInput.value;
+  const team = teamSelect.value;
+  const teamName = teamSelect.selectedOptions[0].text;
 
-console.log(name, teamName);
+  console.log(name, teamName);
 
-//Increment Count
-count++;
-console.log("Total check-ins:", count);
+  //Increment Count
+  count++;
+  console.log("Total check-ins:", count);
 
-// Update attendee count on the page
-const attendeeCountSpan = document.getElementById("attendeeCount");
-attendeeCountSpan.textContent = count;
+  // Update attendee count on the page
+  const attendeeCountSpan = document.getElementById("attendeeCount");
+  attendeeCountSpan.textContent = count;
 
-// Update Progress Bar
-const percentage = Math.round((count / maxCount) * 100) + "%";
-console.log(`Progress: ${percentage}`);
+  // Update Progress Bar
+  const percentage = Math.round((count / maxCount) * 100) + "%";
+  console.log(`Progress: ${percentage}`);
 
-// Team Counter
-const teamCounter = document.getElementById(team + "Count");
-teamCounter.textContent = parseInt(teamCounter.textContent) + 1;
+  const progressBar = document.getElementById("progressBar");
+  progressBar.style.width = percentage;
 
-const message = `Welcome, ${name} from ${teamName}`;
+  // Team Counter
+  const teamCounter = document.getElementById(team + "Count");
+  teamCounter.textContent = parseInt(teamCounter.textContent) + 1;
 
-console.log(message);
+  const message = `Welcome, ${name} from ${teamName}`;
 
-form.reset(); // Reset the form fields
+  console.log(message);
 
+  form.reset(); // Reset the form fields
 });
-
-
-
-
-
-
-
-
-
-
